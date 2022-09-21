e8c2d6eaa69888c889c47d684a3f361d4ca1ead0
unknown <arifbillah779@gmail.com>
2022-09-22 06:53:10 +0800

    fixing the parent relationship for the moving platform and adding some new assets

--- Assets/player/movingPlatform 1/movingPlatform.cs
+++ Assets/player/movingPlatform 1/movingPlatform.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class movingPlatform : MonoBehaviour
 {
     public GameObject Player;
+    private bool isOnPlatform = false;
+
+    void Update()
+    {
+        if(Input.GetKey("space") && isOnPlatform)
+        {
+            Player.transform.parent = null;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
             Player.transform.parent = transform;
+            Player.GetComponent<playerMovement>().moveSpeed = 14;
+            isOnPlatform = true;
         }
     }
 
@@ -18,6 +29,7 @@ public class movingPlatform : MonoBehaviour
     {
         if(other.tag == "Player")
         {
+            Player.GetComponent<playerMovement>().moveSpeed = 7;
             Player.transform.parent = null;
         }        
     }
